af3d0a315564395f8dd08b1e79059753477967f0
AndrewGil <coolman357@gmail.com357>
2019-02-24 19:38:29 -0800

    Fixxing missing files from first commit

--- EECalc/EECalc.cpp
+++ EECalc/EECalc.cpp
@@ -3,19 +3,16 @@
 
 #include "pch.h"
 #include <iostream>
+#include "Tokenizer.hpp"
+#include "Parser.hpp"
+#include "Math.hpp"
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	std::string input = "1W * 10s";
+	EECalc::Tokenizer toker(input);
+	EECalc::Parser parser(toker.extract_tokens());
+	std::cout << parser.next_token();
+	std::cin.ignore();
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ EECalc/Math.hpp
@@ -0,0 +1,99 @@
+#pragma once
+#include "Parser.hpp"
+#include <memory>
+namespace EECalc {
+
+	using default_real_t = double;
+	template<typename Real = default_real_t>
+	class Math {
+	public:
+		struct Value {
+			const Unit unit;
+			virtual Real as_real() = 0;
+			Value(const Unit& uint) : unit(unit) {}
+			Value() : Value(Unit::BaseUnit::Scalar) {}
+			using P = std::unique_ptr<Value>;
+		};
+		struct Constant : Value {
+			const Real value;
+			Real as_real() override {
+				return value;
+			}
+			Constant(Real value, Unit unit = Unit::BaseUnit::Scalar) : Value(unit) {}
+			using P = std::unique_ptr<Constant>;
+		};
+		typename Constant::P evalute(Value& value) {
+			return Constant(value.as_real(), value.unit);
+		}
+		struct BinaryOperation : Value {
+
+			static Unit get_unit(const Value& left, const Value& right) {
+				switch (operation) {
+				case Operator::Add:
+				case Operator::Subtract:
+					if (left.unit == right.unit)
+						return left.unit;
+					throw Unit::UnitMismatchException();
+				case Operator::Multiply:
+					return left.unit * right.unit;
+				case Operator::Divide:
+					return left.unit / right.unit;
+				default:
+					throw std::invalid_argument("unrecognized operator");
+
+				}
+			}
+			enum class Operator {
+				Add,
+				Subtract,
+				Multiply,
+				Divide,
+			};
+			Value::P left;
+			Value::P right;
+			const Operator operation;
+			BinaryOperation(Value::P left, Operator operation, Value::P right) :
+				left(std::move(left)), right(std::move(right)), operation(operation),
+				Value(get_unit(*left, *right)) {
+				
+			}
+			Real as_real() {
+				switch (operation) {
+				case Operator::Add:
+					return left->as_real() + right->as_real();
+				case Operator::Subtract:
+					return left->as_real() - right->as_real();
+				case Operator::Multiply:
+					return left->as_real() * right->as_real();
+				case Operator::Divide:
+					return left->as_real() / right->as_real();
+				default:
+					throw std::logic_error("invalid operator");
+				}
+			}
+		};
+		struct UnaryOperation : Value {
+			enum class Operator {
+				Negate,
+				AbsoluteValue,
+			};
+			Value::P operand;
+			Operator operation;
+			UnaryOperation(Operator operation, typename Value::P operand) :
+				operation(operation), operand(std::move(operand)), Value(operand->unit) {}
+			Real as_real() {
+				switch (operation) {
+				case Operator::Negate:
+					return -operand->as_real();
+				case Operator::AbsoluteValue:
+					Real r = operand->as_real();
+					if (r < 0)
+						return -r;
+					return r;
+				default:
+					throw std::logic_error("invalid unary operator");
+				}
+			}
+		};
+	};
+}
