f425698d1e7a13be1714b436ec0aecab2230f499
AndrewGil <coolman357@gmail.com357>
2019-05-26 19:29:50 -0700

    lots of work done on new scanner and parser

--- src/parser.rs
+++ src/parser.rs
@@ -8,6 +8,7 @@ use crate::scanner::Separator::{OpenParentheses, Comma};
 #[derive(Debug, Clone)]
 pub struct Parser<'a> {
     scanner: Peekable<Scanner<'a>>,
+	workspace: Vec<Node<'a>>
 }
 pub enum ParserError<'a> {
 	ExpectedOperator(&'a Token<'a>),
@@ -33,9 +34,19 @@ pub enum Node<'a> {
 	Variable(&'a str),
 	Function(&'a str,Vec<Node<'a>>),
 	UnaryOperation(UnaryOperator, Box<Node<'a>>),
-	InfixOperation(InfixOperator, Box<Node<'a>>, Box<Node<'a>>)
+	InfixOperation(InfixOperator, Box<Node<'a>>, Box<Node<'a>>),
+	Scope(Node<'a>)
 }
-type ParserResult<'a> = Result<Option<Node<'a>>, ParserError<'a>>;
+type ReduceResult<'a> = Result<&'a Node<'a>, ParserError<'a>>;
+impl InfixOperator {
+	pub fn precedence(&self) -> i32 {
+		match *self {
+			InfixOperator::Minus | InfixOperator::Plus => 3,
+			InfixOperator::Divide | InfixOperator::Times => 2,
+			InfixOperator::Raise => 1
+		}
+	}
+}
 impl<'a> iterator<'a> for Parser<'a> {
 	type Item = Node<'a>;
 	fn next(&mut self) -> Option<Result<Node<'a>, ParserError<'a>>> {
@@ -44,58 +55,15 @@ impl<'a> iterator<'a> for Parser<'a> {
 	}
 
 }
-impl<'a> Node<'a> {
-
-}
+const HIGHEST_PRECEDENCE: i32 = 4;
 impl<'a> Parser<'a> {
 	pub fn from_scanner(scanner: Scanner<'a>) -> Parser<'a> {
-		Parser { scanner: scanner.peekable() }
+		Parser { scanner: scanner.peekable(), last_precedence: HIGHEST_PRECEDENCE }
 	}
-	fn next_node(&mut self, previous: Option<&mut Node<'a>>) -> Result<Node<'a>, ParserError<'a>> {
+	fn shift(&mut self) -> Option<()> {
+		self.workspace.push(self.scanner.next()?)
+	}
+	fn reduce(&mut self) {
 
-	}
-	fn next_unary_operation(&mut self) -> ParserResult {
-		let mut self_clone = self.clone();
-		let unary_op = match self_clone.scanner.next().ok_or_else(EarlyEOF)?.token_type() {
-			TokenType::Operator(Operator::Minus) => Negate,
-			_ => return Some(None)
-		};
-		let operand = Box::new(self_clone.next_node()?);
-		self.clone_from(&self_clone);
-		Ok(Some(UnaryOperation(unary_op, operand)))
-	}
-	fn next_number(&mut self) -> Option<Number> {
-		match self.scanner.peek()?.token_type() {
-			TokenType::Float(f) => Some({self.next_node()?; Number::Float(f) }),
-			TokenType::Int(i) => Some({self.next_node()?; Number::Int(i) }),
-			_ => None
-		}
-	}
-	fn expect_separator(&mut self, sep: Separator) -> Result<(), ParserError<'a>> {
-		match self.scanner.peek().ok_or_else(EarlyEOF)? {
-			TokenType::Separator(sep)=> {self.scanner.next(); Ok(())},
-			tok => Err(ExpectedSeparator(Separator::OpenParentheses, &tok))
-		}
-	}
-	fn next_function(&mut self) -> ParserResult {
-		let mut self_clone = self.clone();
-		let next_token = self_clone.scanner.next();
-		let func_name = match next_token.token_type() {
-			TokenType::Word => next_token.content(),
-			_ => return Some(None)
-		};
-		if self_clone.expect_separator(OpenParentheses).is_err() {
-			return Some(None)
-		}
-		let is_next_closing = |self_clone| self_clone.scanner.peek().ok_or(EarlyEOF)?? != TokenType::Separator(Separator::CloseParentheses);
-		let mut args = Vec::<Node<'a>>::with_capacity(4);
-		while !is_next_closing() {
-			args.push(self_clone.next_node().ok_or(EarlyEOF)??);
-			if !is_next_closing() {
-				self_clone.expect_separator(Comma);
-			}
-		}
-		self.clone_from(&self_clone);
-		Ok(Some(Function {name: func_name, args}))
 	}
 }
